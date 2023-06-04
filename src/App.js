09f16e67c4e0cf1127ccdabcbcc927de0ba6ae39
Arman Garg <armangarg36@gamil.com>
2023-06-04 20:08:26 +0530

    added utm

--- src/App.js
+++ src/App.js
@@ -6,8 +6,37 @@ import courses from './assets/course-main/constants';
 import './App.css'
 import Landing from './containers/landing/Landing';
 import Contact from './containers/contact/Contact';
+import React, { useEffect } from 'react';
 
 const App = () => {
+  
+  useEffect(() => {
+    const saveUTMParams = () => {
+      const queryParams = new URLSearchParams(window.location.search);
+      const utmParams = {};
+
+      for (let param of queryParams) {
+        if (param[0].startsWith('utm_')) {
+          utmParams[param[0]] = param[1];
+        }
+      }
+
+      const jsonString = JSON.stringify(utmParams);
+
+      const fileUrl = process.env.PUBLIC_URL + '/utmParams.json';
+
+      fetch(fileUrl, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonString,
+      });
+    };
+
+    saveUTMParams();
+  }, []);
+
   return (
     <Router>
       <Navbar />
